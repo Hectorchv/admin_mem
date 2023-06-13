4aa1a50f3fb29b1b4015eef90b349d9efa50030f
Hectorchv <hectorchv045@gmail.com>
2023-06-12 20:44:40 -0600

    update

--- mem_vec.c
+++ mem_vec.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-#include<assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
 
 #ifndef MEM
 #define MARCOS_MEM 16
@@ -24,11 +24,11 @@ void insertarList(mem_vec **lista, int dato)
     {
         tmp->marcLibre = dato;
         if (*lista != NULL)
-            tmp->next = *lista;  
+            tmp->next = *lista;
         else
             tmp->next = NULL;
         *lista = tmp;
-    }   
+    }
 }
 
 // Eliminar un elemento en la lista de los
@@ -45,29 +45,34 @@ int extraeList(mem_vec **lista, int marco)
     int dev = 0;
     mem_vec *anterior, *tmp;
     tmp = anterior = (*lista);
-    if (tmp->marcLibre == marco)
+    if (tmp != NULL)
     {
-        dev = 1;
-        *lista = (*lista)->next;
-        free(tmp);
-    }
-    else
-    {
-        tmp = tmp->next;
-        while (tmp != NULL)
+        if (tmp->marcLibre == marco)
         {
-            if(tmp->marcLibre == marco)
+            dev = 1;
+            *lista = (*lista)->next;
+            free(tmp);
+            tmp = anterior = NULL;
+        }
+        else
+        {
+            tmp = tmp->next;
+            while (tmp != NULL)
             {
-                anterior->next = tmp->next;
-                tmp->next = NULL;
-                free(tmp);
-                dev = 1;
-                break;
-            }
-            else
-            {
-                tmp = tmp->next;
-                anterior = anterior->next;
+                if (tmp->marcLibre == marco)
+                {
+                    anterior->next = tmp->next;
+                    tmp->next = NULL;
+                    free(tmp);
+                    dev = 1;
+                    tmp = anterior = NULL;
+                    break;
+                }
+                else
+                {
+                    tmp = tmp->next;
+                    anterior = anterior->next;
+                }
             }
         }
     }
@@ -76,11 +81,11 @@ int extraeList(mem_vec **lista, int marco)
 
 // Agrega el pid al marco de página señalado
 // a la memoria
-void cargarMem(int memoria[MARCOS_MEM],int marcoLibre,int tam, int pid)
+void cargarMem(int memoria[MARCOS_MEM], int marcoLibre, int tam, int pid)
 {
     for (int i = 0; i < tam; i++)
     {
-        memoria[marcoLibre+i] = pid;
+        memoria[marcoLibre + i] = pid;
     }
 }
 
@@ -89,28 +94,28 @@ void cargarMem(int memoria[MARCOS_MEM],int marcoLibre,int tam, int pid)
 //
 // devuelve 1 si lo encontró
 // devuelve 0 si no lo encontró
-// 
+//
 // @param vectorMLibres vector de areas libres
 // @param tam marcos requeridos por el proceso
 int buscEspacio(mem_vec *vectorMLibres[ENT_VECTOR], int tam)
 {
     int encontrado = 0;
-    int indiMarc = (int) log2(tam);
+    int indiMarc = (int)log2(tam);
 
     // Encontramos una area libre del mismo
     // tamaño que requiere el proceso
-    if(vectorMLibres[indiMarc] != NULL)
+    if (vectorMLibres[indiMarc] != NULL)
         encontrado = 1;
-    
+
     // No encontramos uns area libre del mismo
     // tamaño requerido por el proceso
     else
     {
-        for(int i=indiMarc+1;i<ENT_VECTOR;i++)
+        for (int i = indiMarc + 1; i < ENT_VECTOR; i++)
         {
             // Encontramos una area del mayor tamaño
             // requerida por el proceso
-            if(vectorMLibres[i]!=NULL)
+            if (vectorMLibres[i] != NULL)
             {
                 // Dividimos el area cada vez en dos partes
                 // hasta que nos quede una del tamaño requerido
@@ -119,12 +124,12 @@ int buscEspacio(mem_vec *vectorMLibres[ENT_VECTOR], int tam)
                 while (tamArea != indiMarc)
                 {
                     int marcoLibreA = tmp->marcLibre;
-                    int marcoLibreB = tmp->marcLibre+(pow(2,tamArea-1));
+                    int marcoLibreB = tmp->marcLibre + (pow(2, tamArea - 1));
                     eliminarList(&(vectorMLibres[tamArea]));
-                    tamArea --;
-                    insertarList(&(vectorMLibres[tamArea]),marcoLibreA);
-                    insertarList(&(vectorMLibres[tamArea]),marcoLibreB);
-                    tmp=vectorMLibres[tamArea];
+                    tamArea--;
+                    insertarList(&(vectorMLibres[tamArea]), marcoLibreA);
+                    insertarList(&(vectorMLibres[tamArea]), marcoLibreB);
+                    tmp = vectorMLibres[tamArea];
                 }
                 encontrado = 1;
                 break;
@@ -136,21 +141,29 @@ int buscEspacio(mem_vec *vectorMLibres[ENT_VECTOR], int tam)
 
 int reunificarMem(int indiMarc, int tam, mem_vec *vectorMlibres[ENT_VECTOR])
 {
-    int entrada = (int) log2(tam);
+    int entrada = (int)log2(tam);
+    int marcoAUnir = 0;
 
-    if(tam == MARCOS_MEM)
+    if (((indiMarc / tam) % 2) == 0)
+        marcoAUnir = indiMarc + tam;
+    else
+        marcoAUnir = indiMarc - tam;
+
+    if (tam == MARCOS_MEM)
     {
         extraeList(&(vectorMlibres[entrada]), indiMarc);
-        insertarList(vectorMlibres, indiMarc);
+        insertarList(&(vectorMlibres[entrada]), indiMarc);
     }
     else
     {
-        int marcoAUnir = ((indiMarc/(tam*2))*(tam*2)+(tam));
-        if (extraeList(&(vectorMlibres[entrada]),marcoAUnir) != 0)
+        if (extraeList(&(vectorMlibres[entrada]), marcoAUnir) != 0)
         {
-            insertarList(&(vectorMlibres[entrada+1]), (marcoAUnir<indiMarc ? marcoAUnir : indiMarc));
-            reunificarMem((marcoAUnir<indiMarc ? marcoAUnir : indiMarc), tam * 2, vectorMlibres);
+            extraeList(&(vectorMlibres[entrada]), indiMarc);
+            insertarList(&(vectorMlibres[entrada + 1]), ((marcoAUnir < indiMarc) ? marcoAUnir : indiMarc));
+            reunificarMem(((marcoAUnir < indiMarc) ? marcoAUnir : indiMarc), tam * 2, vectorMlibres);
         }
+        else
+            insertarList(&(vectorMlibres[entrada]), indiMarc);
     }
     return 0;
 }
@@ -162,21 +175,23 @@ int desasignarMem(int pid, int memoria[MARCOS_MEM], mem_vec *vectorMlibres[ENT_V
     int flag = 0;
     for (int i = 0; i < MARCOS_MEM; i++)
     {
-        if(memoria[i] == pid)
+        if (memoria[i] == pid)
         {
-            if(flag == 0)
+            if (flag == 0)
                 indiMarc = i;
-            tam ++;
+            tam++;
             flag = 1;
             memoria[i] = -1;
         }
-        else if(flag == 1)
+        else if (flag == 1)
         {
             reunificarMem(indiMarc, tam, vectorMlibres);
             flag = 0;
             tam = 0;
         }
     }
+    if (flag == 1)
+        reunificarMem(indiMarc, tam, vectorMlibres);
     return 1;
 }
 
@@ -184,22 +199,22 @@ int asignarMem(int pid, int tam, mem_vec *vectorMLibres[ENT_VECTOR], int memoria
 {
     int asignado = 0;
     int marcoLibre, a, b;
-    int indiVector = (int) log2(tam);
-    
+    int indiVector = (int)log2(tam);
+
     int espacio = buscEspacio(vectorMLibres, tam);
     if (espacio == 1)
     {
         marcoLibre = vectorMLibres[indiVector]->marcLibre;
         eliminarList(&(vectorMLibres[indiVector]));
-        cargarMem(memoria,marcoLibre,tam,pid);
+        cargarMem(memoria, marcoLibre, tam, pid);
         asignado = 1;
     }
     else if (tam > 1)
     {
-        a = asignarMem(pid, tam/2, vectorMLibres, memoria);
-        b = asignarMem(pid, tam/2, vectorMLibres, memoria);
+        a = asignarMem(pid, tam / 2, vectorMLibres, memoria);
+        b = asignarMem(pid, tam / 2, vectorMLibres, memoria);
     }
-    else if(a == 0 || b ==0)
+    else if (a == 0 || b == 0)
     {
         fprintf(stderr, "Sin espacio en memoria");
     }
@@ -209,53 +224,61 @@ int asignarMem(int pid, int tam, mem_vec *vectorMLibres[ENT_VECTOR], int memoria
 
 int main(int argc, char *argv[])
 {
-    //int pid, tam;
+    int pid, tam;
     FILE *archivo;
     archivo = fopen(argv[1], "r");
     assert(archivo != NULL);
 
+    //Inicializamos la memoria
     int memoria[MARCOS_MEM];
     for (int i = 0; i < MARCOS_MEM; i++)
     {
         memoria[i] = -1;
     }
-    
-    //Vector de areas libre
+
+    // Inicializamos el vector de areas libres
     mem_vec *vectorMLibres[ENT_VECTOR];
-
-    // Inicializamos el vector de areas libre
-    // Con todos los apuntadores a NULL
     for (int i = 0; i < ENT_VECTOR; i++)
-    {
         vectorMLibres[i] = NULL;
-    }
-    // Colocamos el primer elemento de tamaño 16
-    // al vector de áreas libres para inicializar
-    /* insertarList(&(vectorMLibres[0]), 1);
-    insertarList(&(vectorMLibres[0]), 0);
-    insertarList(&(vectorMLibres[1]), 2);
-    insertarList(&(vectorMLibres[2]), 4);
-    insertarList(&(vectorMLibres[3]), 8); */ 
-    insertarList(&(vectorMLibres[1]), 2);
-    insertarList(&(vectorMLibres[1]), 4);
+    insertarList(&(vectorMLibres[4]), 0);
 
-    /* while (!feof(archivo))
+    while (!feof(archivo))
     {
 
         fscanf(archivo, "%d %d", &pid, &tam);
         fgetc(archivo);
 
-        printf("pid: %d tam: %d\n", pid , tam);
-    } */
+        if (tam == -1)
+            desasignarMem(pid, memoria, vectorMLibres);
+        else
+            asignarMem(pid, tam, vectorMLibres, memoria);
 
-    //asignarMem(150, 1, vectorMLibres, memoria);
-    printf("hola mundo\n");
-    
-    //desasignarMem(150, memoria, vectorMLibres);
-    //asignarMem(150, 8, vectorMLibres, memoria);
-    //asignarMem(120, 8, vectorMLibres, memoria);
-    //desasignarMem(150, memoria, vectorMLibres);
-    //reunificarMem(8,4,vectorMLibres);
-
+        printf("*****************************************\n");
+        printf("Situación de la memoria\n\n");
+        printf("N marc\tpid\n");
+        for (int i = 0; i < MARCOS_MEM; i++)
+        {
+            printf("|%d\t|", i);
+            if(memoria[i] == -1)
+                printf(" LIBRE|");
+            else
+                printf(" %5d|", memoria[i]);
+            printf("\n");
+        }
+        printf("----------------------------------------\n");
+        printf("Situacion del vector de areas libres\n\n");
+        printf("Entrada\tMarcos libres\n");
+        for (int i = 0; i < ENT_VECTOR; i++)
+        {
+            mem_vec *tmp = vectorMLibres[i];
+            printf("|%d\t|",i);
+            while (tmp != NULL)
+            {
+                printf("->%d ", tmp->marcLibre);
+                tmp = tmp->next;
+            }
+            printf("\n");
+        }
+    }
     fclose(archivo);
 }
